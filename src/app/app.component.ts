820f44417415e1fbe38892b98e0cab0278127f3b
Luca Bulles <lucabulles@gmail.com>
2023-07-20 16:12:43 +0200

    created header and button components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'superhero-dashboard';
+  title: string = 'superhero-dashboard';
 }
