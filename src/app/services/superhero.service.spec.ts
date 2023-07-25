f99638097e94d0e5732a4fd3e74e1ea8e8708ca4
Luca Bulles <lucabulles@gmail.com>
2023-07-25 16:49:59 +0200

    fixed error about httpmodule inside test file of service

--- src/app/services/superhero.service.spec.ts
+++ src/app/services/superhero.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SuperheroService } from './superhero.service';
 
@@ -6,7 +7,9 @@ describe('SuperheroService', () => {
   let service: SuperheroService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(SuperheroService);
   });
 
