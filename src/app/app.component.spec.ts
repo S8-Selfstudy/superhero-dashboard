ae8c0d513045f4938cb6077cc90fd6b01a390001
Luca Bülles <luca.bulles@alten.nl>
2023-09-07 11:49:14 +0200

    added footer component and added AngularTestingModule to component.spec.ts file of app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,14 +3,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component'; // Import HeaderComponent
 import { ButtonComponent } from './components/button/button.component'; // Import ButtonComponent
 import { SuperheroesComponent } from './components/superheroes/superheroes.component';
+import { AddSuperheroComponent } from './components/add-superhero/add-superhero.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { AddSuperheroComponent } from './components/add-superhero/add-superhero.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent, HeaderComponent, ButtonComponent, SuperheroesComponent, AddSuperheroComponent],
-    imports: [HttpClientModule, RouterModule]
+    declarations: [AppComponent, HeaderComponent, ButtonComponent, SuperheroesComponent, AddSuperheroComponent, FooterComponent],
+    imports: [HttpClientModule, RouterModule, RouterTestingModule]
   }));
 
   it('should create the app', () => {
