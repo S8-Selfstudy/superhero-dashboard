36ab7c11d012c6ab8672b47b988f54b0520a65b7
Luca Bülles <luca.bulles@alten.nl>
2023-09-07 12:00:17 +0200

    add button now only shows on homepage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs'
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,12 +13,16 @@ export class HeaderComponent {
   showAddSuperhero: boolean = false
   subscription!: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value => this.showAddSuperhero = value));
   }
 
   toggleAddHero() {
     this.uiService.toggleAddSuperhero();
+  }
+
+  hasRoute(router: string){
+    return this.router.url === router;
   }
 
 }
