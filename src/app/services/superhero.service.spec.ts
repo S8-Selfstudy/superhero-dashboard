2ea589feb88fc26e83c2ef3e0075ea54a260587e
Luca Bülles <luca.bulles@alten.nl>
2023-09-14 15:33:50 +0200

    added integration tests for service methods get all and delete method

--- src/app/services/superhero.service.spec.ts
+++ src/app/services/superhero.service.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { SuperheroService } from './superhero.service';
+import { Expression } from '@angular/compiler';
 
 describe('SuperheroService', () => {
   let service: SuperheroService;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,4 +18,34 @@ describe('SuperheroService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('GetSuperHeroes should not return a null value', () => {
+    //console.log(service.getSuperheroes());
+    expect(service.getSuperheroes()).not.toBeNull();
+  })
+
+  it('GetSuperHeroes should return an object', () => {
+    const service = TestBed.inject(SuperheroService);
+    const superheroes = service.getSuperheroes();
+    console.log(superheroes);
+  
+    expect(superheroes).toEqual(jasmine.any(Object));
+  });
+
+  it('DeleteSuperHero should return an object', () => {
+    const service = TestBed.inject(SuperheroService);
+    const newHero ={
+      id: 1,
+      name: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      place: 'test',
+      active: true
+    }
+    const superheroes = service.deleteSuperhero(newHero);
+    console.log(superheroes);
+  
+    expect(superheroes).toEqual(jasmine.any(Object));
+  });
+  
 });
